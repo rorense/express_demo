2936e2e78c699ebb31392bb0b5d7fac7b16b0f3a
rorense <rkorense@gmail.com>
2023-09-06 15:17:16 +1200

    first commit

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app = express();
 const PORT = 8000;
 
 app.get('/', (req, res) => {
-    console.log("Hi")
+    const item = {'item': 'clothe'}
+    res.send(item)
 })
 
 app.listen(
     PORT,
-    () => console.log(`Listening on http://localhost:${PORT}`)
+    () => console.log(`Listening on http://localhost:${PORT}/`)
 );
